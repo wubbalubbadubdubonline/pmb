9776e1924c0af805d750be30a4360138f7e2bf6d
wubbalubbadubdubonline <wubbalubbadubdub94@gmail.com>
2016-08-24 00:16:34 -0500

    add cowsay

--- backupbot.py
+++ backupbot.py
@@ -153,6 +153,69 @@ async def on_message(msg):
 				await client.send_message(msg.channel, "http://lmgtfy.com/?q="+'+'.join(q))
 
 
+		elif msg.content.lower().startswith("!cowsay "):
+			stuff = msg.content.split()
+			if len(stuff) > 1:
+				await client.send_message(msg.channel, "```\n"+cowsay(' '.join(stuff[1:]))+"\n```")
+
+
+# A python implementation of cowsay <http://www.nog.net/~tony/warez/cowsay.shtml>
+# Copyright 2011 Jesse Chan-Norris <jcn@pith.org>
+# Licensed under the GNU LGPL version 3.0
+# modified for use in this bot by wubbalubbadubdubonline
+
+import textwrap
+
+def cowsay(str, length=40):
+	return build_bubble(str, length) + build_cow()
+
+def build_cow():
+	return """
+         \   ^__^
+          \  (oo)\_______
+             (__)\       )\/\\
+                 ||----w |
+                 ||     ||
+    """
+
+def build_bubble(str, length=40):
+	bubble = []
+
+	lines = normalize_text(str, length)
+
+	bordersize = len(lines[0])
+
+	bubble.append("  " + "_" * bordersize)
+
+	for index, line in enumerate(lines):
+		border = get_border(lines, index)
+
+		bubble.append("%s %s %s" % (border[0], line, border[1]))
+
+		bubble.append("  " + "-" * bordersize)
+
+	return "\n".join(bubble)
+
+def normalize_text(str, length):
+	lines  = textwrap.wrap(str, length)
+	maxlen = len(max(lines, key=len))
+	return [ line.ljust(maxlen) for line in lines ]
+
+def get_border(lines, index):
+	if len(lines) < 2:
+		return [ "<", ">" ]
+
+	elif index == 0:
+		return [ "/", "\\" ]
+
+	elif index == len(lines) - 1:
+		return [ "\\", "/" ]
+
+	else:
+		return [ "|", "|" ]
+#end licensed content
+
+
 token = ""
 with open("./auth", "r") as f:
 	token = f.read()
